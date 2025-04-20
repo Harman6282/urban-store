63860147071c7fe953e1cbf054ee076106889214
Harman6282 <harmans6282@gmail.com>
2025-04-20 23:10:32 +0530

    product disply sample

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 const HomePage = () => {
-  return <div>Urban store</div>;
+  return <>
+  
+  <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+  
+  </>
 };
 
 export default HomePage;
