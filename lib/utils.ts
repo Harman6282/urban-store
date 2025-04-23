89d1190c71c69f8bb3e616a4c54df2ca30e8b366
Harman6282 <harmans6282@gmail.com>
2025-04-23 14:19:13 +0530

    zod product validation

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,13 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
+// convert prisma object into a regular JS object
 
-// convert prisma object into a regular JS object 
+export function convertToPlainObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value));
+}
 
-export function convertToPlainObject<T>(value: T): T{
-  return JSON.parse(JSON.stringify(value))
+//Format number with decimal places
+
+export function formatNumberWithDecimal(num: number): string {
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * Formats a number to a string with at least two decimal places.
+ * 
+ * @param num - The number to format.
+ * @returns A string representing the number formatted with at least two decimal places.
+ *          If the number has fewer than two decimal places, zeroes are added.
+ */
+
+/*******  d1e73221-b619-45ba-9bcb-98c51b69251b  *******/  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
