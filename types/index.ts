1477e9490b9dfe413693a6c9db69562f230b42a6
Harman6282 <harmans6282@gmail.com>
2025-04-28 17:15:41 +0530

    add to cart schema

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,11 @@
 import {z} from "zod";
-import { insertProductSchema } from "@/lib/constants/validators";
+import { insertProductSchema , insertCartSchema, cartItemSchema } from "@/lib/constants/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
     rating: number;
     createdAt: Date;
 }
+
+export type Cart = z.infer<typeof cartItemSchema>;
+export type CartItem = z.infer<typeof insertCartSchema>;
